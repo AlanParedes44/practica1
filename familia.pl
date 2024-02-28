9058657a9a066bf6d4a62ca3ce85dcc8fb3df0c4
unknown <lpze.alanpablo.paredes.pa@unifranz.edu.bo>
2024-02-28 12:23:08 -0400

    clase 27 de febrero

---
+++ familia.pl
@@ -0,0 +1,36 @@
+cristiano.
+mariel.
+juan.
+batista.
+cristal.
+mario.
+alejandra.
+adrian.
+luciano.
+alvaro.
+
+esHombre(cristiano).
+esHombre(juan).
+esHombre(batista).
+esHombre(mario).
+esHombre(adrian).
+esHombre(luciano).
+esHombre(alvaro).
+
+esMujer(mariel).
+esMujer(cristal).
+esMujer(alejandra).
+
+esHijo(cristiano, juan).
+esHijo(cristiano, batista).
+esHijo(mariel, batista).
+esHijo(cristal, adrian).
+esHijo(batista, adrian).
+esHijo(cristal, luciano).
+esHijo(batista, luciano).
+esHijo(alejandra, alvaro).
+esHijo(mario, alvaro).
+
+esHija(mariel, cristal).
+esHija(batista, alejandra).
+esHija(cristal, alejandra).
